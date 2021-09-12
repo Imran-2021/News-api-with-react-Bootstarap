54bd2a5cd39b861c6492ea8304d8be378f12956e
next-hero <mdimranulhaque.8dec20@gmail.com>
2021-09-12 17:25:02 +0600

    slgka

--- src/Components/TopHeadLIne/TopHeadline.js
+++ src/Components/TopHeadLIne/TopHeadline.js
@@ -6,9 +6,8 @@ const TopHeadline = () => {
     useEffect(() => {
         fetch("https://newsapi.org/v2/everything?q=tesla&from=2021-08-12&sortBy=publishedAt&apiKey=061db73a650d4c92a03a8db9c32816b9")
         .then(req=>req.json())
-        .then(data=>setArticles(data?.articles))
+        .then(data=>setArticles(data.articles))
     }, [])
-    console.log(articles);
     return (
         <div className="p-4">
             <h2>Top headlines : {articles.length}</h2>
